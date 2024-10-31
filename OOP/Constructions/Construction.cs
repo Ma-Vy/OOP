3e5c6b66db7c40e5b1c101af523ffc29c22207e4
Maksym Vyshnevskyi <w69955@student.wsiz.edu.pl>
2024-10-31 11:20:37 +0100

    lab 3

--- OOP/Constructions/Construction.cs
+++ OOP/Constructions/Construction.cs
@@ -1,31 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace OOP.Constructions
+﻿ nnamespace OOP.Constructions
 {
     internal class Construction
     {
+        public Construction(CreateContstructionDTO contstruction)
+        {
+            Height = 23;
+            Width = 43;
+            Entrances = 3;
+            HumanCapacity = 2;
+            BuildMaterial = "Material";
+        }
         public Construction()
         {
-                
+            Height = 23;
+            Width = 43;
+            Entrances = 3;
+            HumanCapacity = 2;
+            BuildMaterial = "Material";
         }
 
         public Construction(float height, float width, int entrances, int humanCapacity, string buildMaterial)
         {
-            Height = height; 
-            Width = width; 
-            Entrances = entrances; 
-            HumanCapacity = humanCapacity; 
-            BuildMaterial = buildMaterial; 
+            Height = height;
+            Width = width;
+            Entrances = entrances;
+            HumanCapacity = humanCapacity;
+            BuildMaterial = buildMaterial;
         }
 
         public float Height { get; set; }
         public float Width { get; set; }
         public int Entrances { get; set; }
         public int HumanCapacity { get; set; }
-        public string BuildMaterial { get; set; }
+        private string _buildMaterial { get; set; }
+        public string BuildMaterial
+        {
+            get
+            {
+                return _buildMaterial;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException("value can not be null");
+                }
+                _buildMaterial = value;
+            }
+        }
     }
 }
